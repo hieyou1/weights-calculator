bf0132a0de43a7374f44326426b0acf5d24afb9f
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2024-12-20 17:43:26 -0600

    pages

--- src/Disclaimer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./css/disclaimer.scss";
-
-export function Disclaimer() {
-    return (<div className="disclaimer">
-        Disclaimer: For educational use only. The grade you see above likely will not be accurate.
-    </div>);
-}
--- src/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./css/header.scss";
-
-export function Header() {
-    return (<div className="header">
-        <h2>Weights Calculator</h2>
-        <h3>"but how many makeups do i REALLY need?"</h3>
-    </div>);
-}
